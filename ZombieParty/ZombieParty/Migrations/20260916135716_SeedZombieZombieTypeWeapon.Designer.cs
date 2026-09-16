ad15432b92e425268ed681ac1974d761b1a3bdb7
ChristopherFortin-Labrie <2586402@cegepmontpetit.ca>
2026-09-16 10:14:49 -0400

    FCT seed zombie zombietype weapon

--- ZombieParty/ZombieParty/Migrations/20260916135716_SeedZombieZombieTypeWeapon.Designer.cs
+++ ZombieParty/ZombieParty/Migrations/20260916135716_SeedZombieZombieTypeWeapon.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ZombieParty.Models.Data;
 
@@ -11,9 +12,11 @@ using ZombieParty.Models.Data;
 namespace ZombieParty.Migrations
 {
     [DbContext(typeof(ZombiePartyDbContext))]
-    partial class ZombiePartyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260916135716_SeedZombieZombieTypeWeapon")]
+    partial class SeedZombieZombieTypeWeapon
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -120,18 +123,6 @@ namespace ZombieParty.Migrations
                             Force = 10m,
                             Image = "https://publicdomainvectors.org/photos/Shield-ClassicMedieval1.png",
                             Name = "Master Shield",
-                            Price = 500m,
-                            Qty = 30,
-                            QtyBought = 1
-                        },
-                        new
-                        {
-                            WeaponId = 3,
-                            CreatedDate = new DateTime(1999, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
-                            Description = "A less awesome Stainless Steel Shield",
-                            Force = 10m,
-                            Image = "https://publicdomainvectors.org/photos/Shield-ClassicMedieval1.png",
-                            Name = "Lesser Shield",
                             Price = 500m,
                             Qty = 30,
                             QtyBought = 1
